4f1919e7dcd43777ad9680f0c6d84ca81696a42f
JAKADSD <154687283+JAKADSD@users.noreply.github.com>
2024-05-05 01:53:33 +0200

    Create Yay.lua

---
+++ Yay.lua
@@ -0,0 +1,28 @@
+
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/trendyylol/holder/main/cattoware"))()
+
+local window = library:CreateWindow("LunaWare🐉", Vector2.new(300, 300), Enum.KeyCode.L)
+local tab = window:CreateTab("Main")
+local tab2 = window:CreateTab("Msc")
+local watermark = library:CreateWatermark("LunarWare on top!  |{fps}| {game}")
+
+local sector2 = tab:CreateSector("Lunarware shit")
+local button = sector2:AddButton("LunarWare aim lock", function(bool)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JAKADSD/Lunar/main/Shh.lua"))()
+end) 
+
+local button = sector2:AddButton("LunarWare Cam", function(bool)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JAKADSD/Lunar/main/Uh.lua"))()
+end)
+local sector3 = tab:CreateSector("Rndm")
+local button = sector3:AddButton("Mob res", function(bool)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JAKADSD/Lunar/main/RES.lua"))()
+end)
+local button = sector3:AddButton("Speed", function(bool)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scripthubekitten/backupspeedtool/main/backupspeedtool", true))()
+end)
+
+local button = sector3:AddButton("antilock not mine", function(bool)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/wenny69420/KirbswareScripts/main/MobileV3'))()
+end)
